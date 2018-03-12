e372c23317c9ad41e1a6dd081c0cdd327c0a200a
Danny Luk <luk036@gmail.com>
2018-03-12 16:54:28 +0800

    ci testing

--- test_proj_geom.py
+++ test_proj_geom.py
@@ -1,6 +1,6 @@
 from proj_geom import * 
 
 def test_incident():
-    p = pg_point([1-2j, 3-1j, 2+1j]) # complex number
+    p = pg_point([1-4j, 3-1j, 2+1j]) # complex number
     q = pg_point([-2+1j, 1-3j, -1-1j])
     assert p.incident(p*q)
