bb95790c29dd7112486bc30974f5e7b1a41cc530
Live session user <luk036@gmail.com>
2018-03-29 19:59:06 +0800

    comment out no_test

--- tests/test_proj_plane.py
+++ tests/test_proj_plane.py
@@ -53,40 +53,40 @@ def test_complex_line():
     check_desargue(l, m, r, s, t, u)
 
 
-def no_test_symbolic():
-    import sympy
-    sympy.init_printing()
-    pv = sympy.symbols("p:3", integer=True)
-    qv = sympy.symbols("q:3", integer=True)
-    lambda1, mu1 = sympy.symbols("lambda1 mu1", integer=True)
-    p = pg_point(pv)
-    q = pg_point(qv)
-    r = plucker(lambda1, p, mu1, q)
-    sv = sympy.symbols("s:3", integer=True)
-    tv = sympy.symbols("t:3", integer=True)
-    lambda2, mu2 = sympy.symbols("lambda2 mu2", integer=True)
-    s = pg_point(sv)
-    t = pg_point(tv)
-    u = plucker(lambda2, s, mu2, t)
+# def no_test_symbolic():
+#     import sympy
+#     sympy.init_printing()
+#     pv = sympy.symbols("p:3", integer=True)
+#     qv = sympy.symbols("q:3", integer=True)
+#     lambda1, mu1 = sympy.symbols("lambda1 mu1", integer=True)
+#     p = pg_point(pv)
+#     q = pg_point(qv)
+#     r = plucker(lambda1, p, mu1, q)
+#     sv = sympy.symbols("s:3", integer=True)
+#     tv = sympy.symbols("t:3", integer=True)
+#     lambda2, mu2 = sympy.symbols("lambda2 mu2", integer=True)
+#     s = pg_point(sv)
+#     t = pg_point(tv)
+#     u = plucker(lambda2, s, mu2, t)
 
-    # Prove Pappus Theorem
-    G = meet(join(p, t), join(q, s))
-    H = meet(join(p, u), join(r, s))
-    I = meet(join(q, u), join(r, t))
-    ans = G.dot(join(H, I))
-    ans = sympy.simplify(ans)
-    assert ans == 0
+#     # Prove Pappus Theorem
+#     G = meet(join(p, t), join(q, s))
+#     H = meet(join(p, u), join(r, s))
+#     I = meet(join(q, u), join(r, t))
+#     ans = G.dot(join(H, I))
+#     ans = sympy.simplify(ans)
+#     assert ans == 0
 
-    # p, q, s, t
-    lambda3, mu3 = sympy.symbols("lambda3 mu3", integer=True)
-    p2 = plucker(lambda1, p, mu1, t)
-    q2 = plucker(lambda2, q, mu2, t)
-    s2 = plucker(lambda3, s, mu3, t)
+#     # p, q, s, t
+#     lambda3, mu3 = sympy.symbols("lambda3 mu3", integer=True)
+#     p2 = plucker(lambda1, p, mu1, t)
+#     q2 = plucker(lambda2, q, mu2, t)
+#     s2 = plucker(lambda3, s, mu3, t)
 
-    # Prove Desargue Theorem
-    G = meet(join(p, q), join(p2, q2))
-    H = meet(join(q, s), join(q2, s2))
-    I = meet(join(s, p), join(s2, p2))
-    ans = G.dot(join(H, I))
-    ans = sympy.simplify(ans)
-    assert ans == 0
+#     # Prove Desargue Theorem
+#     G = meet(join(p, q), join(p2, q2))
+#     H = meet(join(q, s), join(q2, s2))
+#     I = meet(join(s, p), join(s2, p2))
+#     ans = G.dot(join(H, I))
+#     ans = sympy.simplify(ans)
+#     assert ans == 0
