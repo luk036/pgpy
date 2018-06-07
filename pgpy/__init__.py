5d471ac79b6211e3f91f74f31bea09aae3a6f2d9
Live session user <luk036@gmail.com>
2018-06-07 14:46:26 +0800

    restructure the package

---
+++ pgpy/__init__.py
@@ -0,0 +1,17 @@
+"""
+PgPy
+=====
+"""
+
+from __future__ import absolute_import
+
+import sys
+if sys.version_info[:2] < (2, 7):
+    m = "Python 2.7 or later is required for NetworkX (%d.%d detected)."
+    raise ImportError(m % sys.version_info[:2])
+del sys
+
+from pgpy.proj_plane import *
+from pgpy.proj_line import *
+from pgpy.ck_plane import *
+from pgpy.persp_plane import *
