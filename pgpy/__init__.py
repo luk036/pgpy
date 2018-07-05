33d160d3e2c5e05fd271d1284aba4c1e8aa6dfd6
Wai-Shing Luk <luk036@gmail.com>
2018-07-05 12:53:03 +0800

    Update __init__.py

--- pgpy/__init__.py
+++ pgpy/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 
 import sys
 if sys.version_info[:2] < (2, 7):
-    m = "Python 2.7 or later is required for NetworkX (%d.%d detected)."
+    m = "Python 2.7 or later is required for PgPy (%d.%d detected)."
     raise ImportError(m % sys.version_info[:2])
 del sys
 
